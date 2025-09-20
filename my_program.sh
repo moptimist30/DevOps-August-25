4893b443c3e7497a6b1aecd952809a52b460cf72
EC2 Default User <ec2-user@ip-172-31-45-214.ap-south-1.compute.internal>
2025-09-20 07:05:06 +0000

    pushing changes from local

---
+++ my_program.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Read first number
+echo -n "Enter the first number:"
+read num1
+
+# Read second number
+echo -n "Enter the second number:"
+read num2
+
+#Add the numbers
+sum=$((num1 + num2))
+
+# Display the result
+echo "The sum of $num1 and $num2 is: $sum"
+
+echo "This is env variable test: $TEST"
+
+
+
